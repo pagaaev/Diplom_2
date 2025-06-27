e41d7091c50e7b7f3e442743288914043e2a2626
Artur Pagaev <paagaev@mail.ru>
2025-06-27 10:22:37 +0700

    refactor: unify baseURI setup and move user deletion to @After
    - RestAssured.baseURI задан один раз в статическом блоке, чтобы не повторять в каждом тесте
    - Перенес удаление пользователя в @After метод, чтобы удаление выполнялось всегда, даже если тест упал
    - Добавлены проверки сообщений об ошибках в негативных тестах createUser

--- src/test/java/CreateUserTest.java
+++ src/test/java/CreateUserTest.java
@@ -4,18 +4,35 @@ import io.restassured.response.Response;
 import org.example.User;
 import org.example.UserAPI;
 import org.example.UserUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CreateUserTest {
+
     private UserAPI userAPI;
+    private String accessToken;
+
+    // Базовый URI можно задать один раз в статическом блоке
+    static {
+        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
+    }
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
         userAPI = new UserAPI();
+        accessToken = null;
+    }
+
+    @After
+    public void tearDown() {
+        // Удаляем пользователя, если accessToken получен
+        if (accessToken != null) {
+            userAPI.deleteUser(accessToken);
+        }
     }
 
     @Test
@@ -23,11 +40,11 @@ public class CreateUserTest {
     public void createNewUserTest() {
         User user = UserUtils.getRandomUser();
         Response response = userAPI.createUser(user);
-        response.then().assertThat().body("success", equalTo(true))
-                .and()
-                .statusCode(SC_OK);
-        String accessToken = response.path("accessToken");
-        userAPI.deleteUser(accessToken);
+        response.then().assertThat()
+                .statusCode(SC_OK)
+                .body("success", equalTo(true));
+
+        accessToken = response.path("accessToken");
     }
 
     @Test
@@ -35,12 +52,13 @@ public class CreateUserTest {
     public void createRegisteredUserTest() {
         User user = UserUtils.getRandomUser();
         Response createResponse = userAPI.createUser(user);
+        accessToken = createResponse.path("accessToken");
+
         Response createDoubleResponse = userAPI.createUser(user);
-        createDoubleResponse.then().assertThat().body("success", equalTo(false))
-                .and()
-                .statusCode(SC_FORBIDDEN);
-        String accessToken = createResponse.path("accessToken");
-        userAPI.deleteUser(accessToken);
+        createDoubleResponse.then().assertThat()
+                .statusCode(SC_FORBIDDEN)
+                .body("success", equalTo(false))
+                .body("message", equalTo("User already exists"));  // пример сообщения об ошибке
     }
 
     @Test
@@ -49,9 +67,10 @@ public class CreateUserTest {
         User user = UserUtils.getRandomUser();
         user.setEmail(null);
         Response createResponse = userAPI.createUser(user);
-        createResponse.then().assertThat().body("success", equalTo(false))
-                .and()
-                .statusCode(SC_FORBIDDEN);
+        createResponse.then().assertThat()
+                .statusCode(SC_FORBIDDEN)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));  // пример сообщения
     }
 
     @Test
@@ -60,9 +79,10 @@ public class CreateUserTest {
         User user = UserUtils.getRandomUser();
         user.setPassword(null);
         Response createResponse = userAPI.createUser(user);
-        createResponse.then().assertThat().body("success", equalTo(false))
-                .and()
-                .statusCode(SC_FORBIDDEN);
+        createResponse.then().assertThat()
+                .statusCode(SC_FORBIDDEN)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));  // пример сообщения
     }
 
     @Test
@@ -71,8 +91,9 @@ public class CreateUserTest {
         User user = UserUtils.getRandomUser();
         user.setName(null);
         Response createResponse = userAPI.createUser(user);
-        createResponse.then().assertThat().body("success", equalTo(false))
-                .and()
-                .statusCode(SC_FORBIDDEN);
+        createResponse.then().assertThat()
+                .statusCode(SC_FORBIDDEN)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));  // пример сообщения
     }
 }
